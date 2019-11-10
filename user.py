0b6d0def203efc824066a87fba3c13f507fa322c
michael <mikelkarije@gmail.com>
2019-11-10 22:54:12 +0300

    addition of user list

--- user.py
+++ user.py
@@ -1,3 +1,10 @@
+import pyperclip
+
 
 class user
  
+    '''
+    this class generates instance of new users
+    '''
+    user_list = []
+    def __init__(self, account ,username , email, password  ):

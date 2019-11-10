dac92180213ca27e65835c16b703d440853992d0
michael <mikelkarije@gmail.com>
2019-11-10 23:31:33 +0300

    commit has new methods for saving users and deleting user accounts

--- user.py
+++ user.py
@@ -1,14 +1,38 @@
 import pyperclip
 
-
-class user
+class User:
  
     '''
     this class generates instance of new users
     '''
     user_list = []
     def __init__(self, account ,username , email, password):
-        self.account =account,
+    
         self.username = username,
         self.email = email,
         self.password = password,
+    def save_user(self):
+        '''
+        
+        '''
+        User.user_list.append(self)
+    def delete_user(self):
+        '''
+        
+        '''
+        User.user_list.remove(self)
+    @classmethod
+    def find_by_name(cls,username):
+        '''
+        method that checks the username of the user and matches it to the one given.
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return user
+
+    @classmethod
+    def display_user(cls):
+        '''
+        returns user list
+        '''
+        return cls.user_list

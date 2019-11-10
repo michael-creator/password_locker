4d428fece14fdd3e9e795d6e4ce3443d586fff6e
michael <mikelkarije@gmail.com>
2019-11-10 23:32:23 +0300

    added user list for testing

--- user_test.py
+++ user_test.py
@@ -1,4 +1,22 @@
 import unittest
-from user import user
+from user import User
 
-class testUser(unittest.TestCase):
+class TestUser(unittest.TestCase):
+    '''
+    test class that determines test case for user class behaviours.
+    '''
+    def setup(self):
+        '''
+        
+        '''
+        self.new_user = User("facebook""mickey","mikelkarije@gmail.com", "mickey22")
+        
+        self.assertEqual(self.new_user.account,)
+        self.assertEqual(self.new_user.user_name,)
+        self.assertEqual(self.new_user.email,)
+        self.assertEqual(self.new_user.password,) 
+        
+        
+        
+        if __name__ ==  '__main__':
+            unittest.main()

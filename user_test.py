82fe1b17f01762236a38126bc1b0039b9402aef2
michael <mikelkarije@gmail.com>
2019-11-10 23:43:35 +0300

    update new function in the user test file to save user credentials

--- user_test.py
+++ user_test.py
@@ -15,7 +15,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_name,)
         self.assertEqual(self.new_user.email,)
         self.assertEqual(self.new_user.password,) 
+    def test_save_user(self):
         
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(self),1)
         
         
         if __name__ ==  '__main__':

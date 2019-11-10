62013d3440e1a5b09041aa7a8019a3dd4aef5517
michael <mikelkarije@gmail.com>
2019-11-10 23:03:32 +0300

    this commit adds more detailed data on what the user list requiress

--- user.py
+++ user.py
@@ -7,4 +7,8 @@ class user
     this class generates instance of new users
     '''
     user_list = []
-    def __init__(self, account ,username , email, password  ):
+    def __init__(self, account ,username , email, password):
+        self.account =account,
+        self.username = username,
+        self.email = email,
+        self.password = password,

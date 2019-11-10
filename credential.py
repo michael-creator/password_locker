bacf3f20066040636d5bb00ba8370cfb4129c07a
michael <mikelkarije@gmail.com>
2019-11-11 00:23:16 +0300

    import of pyperclip, user class,string and random password generator

---
+++ credential.py
@@ -0,0 +1,9 @@
+import random
+import string 
+import pyperclip
+
+class Credential:
+    #  credential_list =  []
+    #  def __init__(self, app_title, password):
+    #      self.app_title = app_title
+    #      self.password = password

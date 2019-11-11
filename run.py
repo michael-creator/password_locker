280cda1c83fee28a1a4b55e614f306cc6695bc7b
michael <mikelkarije@gmail.com>
2019-11-11 13:27:09 +0300

    commit will add the def function for the main app aand its data

--- run.py
+++ run.py
@@ -28,6 +28,24 @@ def display_user():
     function that displays users
     ''' 
     user.display_user()
+def display_credential():
+    '''
+    function that displays users credentials
+    ''' 
+    return Credential.display_credential()
+def main():
+    print("Hello welcome to password locker")
+    print("lets begin to create an account")
+    print("_"*25)
+    print("i. create Account")
+    print("Enter username")
+    user = input()  
+    print("Enter password")
+    password = input()
+    # save_users(create_user(user , email ,password))
+    print(f"Welcome {user} you have created an account with us, please enjoy")
+    print("press (a)login into an account, or (b) exit the program")
+
 
     
     

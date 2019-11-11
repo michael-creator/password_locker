03f4cfa325a308e9195c8de623d9b6c155f1b28e
michael <mikelkarije@gmail.com>
2019-11-11 11:52:42 +0300

    commit adds def creat user in the run file to be executed

---
+++ run.py
@@ -0,0 +1,14 @@
+import pyperclip
+from credential import Credential
+from user import User
+
+
+def create_user(user_name,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(user_name ,password  )
+
+
+    
+  
